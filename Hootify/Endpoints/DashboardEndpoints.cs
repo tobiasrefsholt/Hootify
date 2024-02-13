5b412b66bd5f7832eb84105b87fc174063f62502
tobias <tobias@refsholt.net>
2024-02-13 18:34:29 +0100

    Better endpoint naming

--- Hootify/Endpoints/DashboardEndpoints.cs
+++ Hootify/Endpoints/DashboardEndpoints.cs
@@ -11,20 +11,20 @@ public static class DashboardEndpoints
     {
         return builder.UseEndpoints(endpoints =>
         {
-            endpoints.MapPost("/dashboard/getQuestion", (Guid id, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/questions/get", (Guid id, AppDbContext dbContext) =>
             {
                 var questionService = new DashboardQuestionService(dbContext);
                 return questionService.Get(id);
             });
 
-            endpoints.MapPost("/dashboard/getQuestions",
+            endpoints.MapPost("/dashboard/questions/getAll",
                 (QuestionFilterOptions filterOptions, AppDbContext dbContext) =>
                 {
                     var questionService = new DashboardQuestionService(dbContext);
                     return questionService.GetAll();
                 }).RequireAuthorization();
 
-            endpoints.MapPost("/dashboard/addQuestion",
+            endpoints.MapPost("/dashboard/questions/add",
                 (QuestionWithAnswer question, AppDbContext dbContext, HttpContext httpContext) =>
                 {
                     var questionService = new DashboardQuestionService(dbContext);
@@ -34,25 +34,25 @@ public static class DashboardEndpoints
                     questionService.Add(question, userId);
                 });
 
-            endpoints.MapPost("/dashboard/deleteQuestion", (QuestionWithAnswer question, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/questions/delete", (QuestionWithAnswer question, AppDbContext dbContext) =>
             {
                 var questionService = new DashboardQuestionService(dbContext);
                 questionService.Delete(question.Id);
             });
 
-            endpoints.MapPost("/dashboard/editQuestion", (QuestionWithAnswer question, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/questions/edit", (QuestionWithAnswer question, AppDbContext dbContext) =>
             {
                 var questionService = new DashboardQuestionService(dbContext);
                 questionService.Update(question);
             });
 
-            endpoints.MapPost("/dashboard/getCategories", (AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/categories/getAll", (AppDbContext dbContext) =>
             {
                 var categoryService = new DashboardCategoryService(dbContext);
                 return categoryService.GetAll();
             });
 
-            endpoints.MapPost("/dashboard/addCategory",
+            endpoints.MapPost("/dashboard/categories/add",
                 (Category category, AppDbContext dbContext, HttpContext httpContext) =>
                 {
                     var categoryService = new DashboardCategoryService(dbContext);
@@ -62,19 +62,19 @@ public static class DashboardEndpoints
                     categoryService.Add(category, userId);
                 });
 
-            endpoints.MapPost("/dashboard/deleteCategory", (Category category, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/categories/delete", (Category category, AppDbContext dbContext) =>
             {
                 var categoryService = new DashboardCategoryService(dbContext);
                 categoryService.Delete(category.Id);
             });
 
-            endpoints.MapPost("/dashboard/editCategory", (Category category, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/categories/edit", (Category category, AppDbContext dbContext) =>
             {
                 var categoryService = new DashboardCategoryService(dbContext);
                 categoryService.Update(category);
             });
 
-            endpoints.MapPost("/dashboard/createQuiz", (Quiz quiz, AppDbContext dbContext, HttpContext httpContext) =>
+            endpoints.MapPost("/dashboard/quiz/add", (Quiz quiz, AppDbContext dbContext, HttpContext httpContext) =>
             {
                 var quizService = new DashboardQuizService(dbContext);
                 var success = Guid.TryParse(httpContext.User.FindFirstValue(ClaimTypes.NameIdentifier),
@@ -83,25 +83,25 @@ public static class DashboardEndpoints
                 quizService.Add(quiz, userId);
             });
             
-            endpoints.MapPost("/dashboard/getQuiz", (Guid id, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/quiz/get", (Guid id, AppDbContext dbContext) =>
             {
                 var quizService = new DashboardQuizService(dbContext);
                 return quizService.Get(id);
             });
 
-            endpoints.MapPost("/dashboard/getQuizzes", (AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/quiz/getAll", (AppDbContext dbContext) =>
             {
                 var quizService = new DashboardQuizService(dbContext);
                 return quizService.GetAll();
             });
 
-            endpoints.MapPost("/dashboard/deleteQuiz", (Quiz quiz, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/quiz/delete", (Quiz quiz, AppDbContext dbContext) =>
             {
                 var quizService = new DashboardQuizService(dbContext);
                 quizService.Delete(quiz.Id);
             });
 
-            endpoints.MapPost("/dashboard/editQuiz", (Quiz quiz, AppDbContext dbContext) =>
+            endpoints.MapPost("/dashboard/quiz/edit", (Quiz quiz, AppDbContext dbContext) =>
             {
                 var quizService = new DashboardQuizService(dbContext);
                 quizService.Update(quiz);
