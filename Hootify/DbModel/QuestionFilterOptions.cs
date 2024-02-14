db376753c3f11b1db73db30b3b5885c78baaff2b
tobias <tobias@refsholt.net>
2024-02-14 13:21:46 +0100

    Changed models

--- Hootify/DbModel/QuestionFilterOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace Hootify.DbModel;
-
-public class QuestionFilterOptions
-{
-    public Guid CategoryId { get; set; }
-    public string Search { get; set; }
-}
---
+++ Hootify/ViewModel/Game.cs
@@ -0,0 +1,7 @@
+namespace Hootify.ViewModel;
+
+public class Game
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+}
