55b8d6d2612d0ba204a671d08f2f0dee3bad4f60
tobias <tobias@refsholt.net>
2024-02-23 12:50:00 +0100

    Call getGameState when time runs out

--- frontend/src/pages/gamePage/GamePage.tsx
+++ frontend/src/pages/gamePage/GamePage.tsx
@@ -7,14 +7,18 @@ import {ShowAnswer} from "./ShowAnswer";
 
 export default function GamePage() {
     const playerId = useParams().playerId || "";
-    const { gameState, players, question, questionWithAnswer, answerQuestion, sendChatMessage } = useWebSocket(playerId);
+    const {
+        gameState,
+        players,
+        question,
+        questionWithAnswer,
+        getGameState,
+        answerQuestion,
+        sendChatMessage
+    } = useWebSocket(playerId);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen gap-10 px-10">
-            {gameState ===  null && <h1 className="text-6xl text-center font-bold">Connecting to server...</h1>}
-            {gameState === GameState.WaitingForPlayers && <WaitingForPlayers players={players} currentPlayerId={playerId} sendChatMessage={sendChatMessage} />}
-            {gameState === GameState.QuestionInProgress && <ShowQuestion question={question} answerQuestion={answerQuestion} />}
-            {gameState === GameState.QuestionComplete && <ShowAnswer question={questionWithAnswer} />}
             {gameState === null && <h1 className="text-6xl text-center font-bold">Connecting to server...</h1>}
             {gameState === GameState.WaitingForPlayers &&
                 <WaitingForPlayers
@@ -26,6 +30,7 @@ export default function GamePage() {
                 <ShowQuestion
                     question={question}
                     answerQuestion={answerQuestion}
+                    getGameState={getGameState}
                 />}
             {gameState === GameState.QuestionComplete && <ShowAnswer question={questionWithAnswer}/>}
             {gameState === GameState.GameComplete && <h1 className="text-6xl text-center font-bold">Game over!</h1>}
