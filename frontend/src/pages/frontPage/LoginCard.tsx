9b2d0cd6be74977bddf16510972970d8c07fa467
tobias <tobias@refsholt.net>
2024-02-25 00:58:23 +0100

    Hide login for if user is logged inn

--- frontend/src/pages/frontPage/LoginCard.tsx
+++ frontend/src/pages/frontPage/LoginCard.tsx
@@ -1,24 +1,46 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter} from "@/components/ui/card";
+import {Input} from "@/components/ui/input";
+import {useUser} from "@/context/userContext.tsx";
+import {useNavigate} from "react-router";
 
 
 export default function JoinGameCard() {
+    const user = useUser();
+    const navigate = useNavigate();
     return (
-        <Card className="w-80">
-            <form action="">
-                <CardHeader>
-                    <CardTitle>Login</CardTitle>
-                    <CardDescription>Access dashboard</CardDescription>
-                </CardHeader>
-                <CardContent className="flex flex-col gap-2">
-                    <Input placeholder="Email" inputMode="numeric" />
-                    <Input placeholder="Password" type="password" />
-                </CardContent>
-                <CardFooter className="mt-auto">
-                    <Button variant="secondary">Login</Button>
-                </CardFooter>
-            </form>
+        <Card className="w-80 flex-col">
+            {!user.userData?.id ?
+                <>
+                    <CardHeader>
+                        <CardTitle>Welcome back, {user.userData?.name}</CardTitle>
+                        <CardDescription>Access dashboard</CardDescription>
+                    </CardHeader>
+                    <CardContent className="grow">
+                        <p>You are logged in</p>
+                    </CardContent>
+                    <CardFooter className="gap-2">
+                        <Button variant="secondary" onClick={() => navigate("/dashboard")}>
+                            Open dashboard
+                        </Button>
+                        <Button variant="outline" onClick={user.logout}>Logout</Button>
+                    </CardFooter>
+                </>
+                :
+                <form action="">
+                    <CardHeader>
+                        <CardTitle>Login</CardTitle>
+                        <CardDescription>Access dashboard</CardDescription>
+                    </CardHeader>
+                    <CardContent className="flex flex-col gap-2">
+                        <Input placeholder="Email" inputMode="numeric"/>
+                        <Input placeholder="Password" type="password"/>
+                    </CardContent>
+                    <CardFooter className="mt-auto">
+                        <Button variant="secondary">Login</Button>
+                    </CardFooter>
+                </form>
+            }
         </Card>
     )
 }
