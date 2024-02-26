066b20d601e9bc5731130f47429a26e5e1da70f8
tobias <tobias@refsholt.net>
2024-02-26 18:08:10 +0100

    Add unauthorized message to dashboard

--- frontend/src/pages/dashboardPage/DashboardPage.tsx
+++ frontend/src/pages/dashboardPage/DashboardPage.tsx
@@ -4,8 +4,12 @@ import GamesPage from "@/pages/dashboardPage/gamesPage/GamesPage.tsx";
 import IndexPage from "@/pages/dashboardPage/indexPage/indexPage.tsx";
 import QuestionsPage from "@/pages/dashboardPage/questionsPage/questionsPage.tsx";
 import QuizzesPage from "@/pages/dashboardPage/quizzesPage/QuizzesPage.tsx";
+import {useUser} from "@/context/userContext.tsx";
 
 export default function DashboardPage() {
+    const {userData} = useUser();
+    if (!userData?.email) return "Unauthorized";
+
     return (
         <div className="flex gap">
             <Sidebar/>
