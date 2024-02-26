cb3ea91d6a4476afdf17e502606f82ce83d73af7
tobias <tobias@refsholt.net>
2024-02-26 12:45:30 +0100

    Sidebar design

--- frontend/src/pages/dashboardPage/sidebar/Sidebar.tsx
+++ frontend/src/pages/dashboardPage/sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import Navigation from "@/pages/dashboardPage/sidebar/Navigation.tsx";
 
 export default function Sidebar() {
     return (
-        <div className="h-screen w-80 bg-secondary">
+        <div className="h-screen w-80 border-r">
             <Navigation/>
         </div>
     )
