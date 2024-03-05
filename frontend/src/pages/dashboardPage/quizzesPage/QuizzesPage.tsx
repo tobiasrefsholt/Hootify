e249aba5cc7cb7e63f1e19813e2c15393fe2966a
tobias <tobias@refsholt.net>
2024-03-05 14:47:27 +0100

    Use quiz context instead if fetching separately

--- frontend/src/pages/dashboardPage/quizzesPage/QuizzesPage.tsx
+++ frontend/src/pages/dashboardPage/quizzesPage/QuizzesPage.tsx
@@ -1,29 +1,21 @@
 import PageContainer from "@/components/ui/pageContainer";
 import PageHeader from "@/components/ui/pageHeader.tsx";
-import {useFetch} from "@/hooks/useFetch.ts";
-import {ApiEndpoint, Quiz} from "@/Types.ts";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {DataTable} from "@/components/ui/dataTable/DataTable.tsx";
 import {tableColumns} from "@/pages/dashboardPage/quizzesPage/quizzesTable/TableColumns.tsx";
 import {RowSelectionState} from "@tanstack/react-table";
+import {useQuizzes} from "@/context/quizzesContext.tsx";
 
 export default function QuizzesPage() {
-    const quizzes = useFetch<Quiz[]>(ApiEndpoint.DashboardGetAllQuizzes, []);
+    const {quizzes} = useQuizzes();
     const [selectedQuizzes, setSelectedQuizzes] = useState<RowSelectionState>({});
-
-    useEffect(() => {
-        quizzes.doFetch("POST", [], {
-            categoryId: "3fa85f64-5717-4562-b3fc-2c963f66afaf",
-            search: ""
-        });
-    }, []);
 
     return (
         <PageContainer>
             <PageHeader>Quizzes</PageHeader>
             <DataTable
                 columns={tableColumns}
-                data={quizzes.data || []}
+                data={quizzes || []}
                 rowSelection={selectedQuizzes}
                 setRowSelection={setSelectedQuizzes}
             />
