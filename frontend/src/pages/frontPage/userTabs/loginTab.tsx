a8260f8a7dd4a8aa1e9bab7a9378a54a14d0e849
tobias <tobias@refsholt.net>
2024-03-09 18:20:42 +0100

    Redesign register and login forms

--- frontend/src/pages/frontPage/userTabs/loginTab.tsx
+++ frontend/src/pages/frontPage/userTabs/loginTab.tsx
@@ -1,63 +1,102 @@
-import {FormEvent, useRef, useState} from "react";
+import {useState} from "react";
 import {Button} from "@/components/ui/button.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {useUser} from "@/context/userContext.tsx";
 import {useNavigate} from "react-router";
 import {Loader2} from "lucide-react";
 import {ApiEndpoint} from "@/Types.ts";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.tsx";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const FormSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1, "Password cannot be empty")
+});
 
 export default function LoginTab() {
     const user = useUser();
     const navigate = useNavigate();
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
 
     const [isPending, setIsPending] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
-    async function handleLogin(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    })
+
+    async function handleLogin(data: z.infer<typeof FormSchema>) {
         setIsPending(true);
         setError(null);
-        const email = emailRef.current?.value;
-        const password = passwordRef.current?.value;
-        if (!email && !password) return;
 
-        const response = await fetch(import.meta.env.VITE_BACKEND_URL + ApiEndpoint.Login, {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({email, password})
-        });
-
-        setIsPending(false);
-
-        if (!response.ok) {
-            setError("Login failed, please try again.");
+        try {
+            const response = await fetch(import.meta.env.VITE_BACKEND_URL + ApiEndpoint.Login, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+            setIsPending(false);
+            if (response.status === 401) {
+                setError("Wrong username or password.");
+                return;
+            }
+            if (response.status !== 200) {
+                throw new Error();
+            }
+        } catch (e) {
+            setError("Something went wrong. Please try again later.");
+            setIsPending(false);
             return;
         }
 
-        user.login({email: email!});
+        setIsPending(false);
+
+        user.login({email: data.email});
         navigate("/dashboard");
     }
 
     return (
-        <>
-            <form onSubmit={handleLogin} className="space-y-2.5">
-                <Input placeholder="Email" inputMode="numeric" ref={emailRef}/>
-                <Input placeholder="Password" type="password" ref={passwordRef}/>
-                {error && <p>{error}</p>}
-                <div className="space-x-2.5">
-                    <Button variant="secondary">
-                        {isPending
-                            ? <><Loader2/> Logging in</>
-                            : "Login"}
-                    </Button>
-                    <Button variant="ghost">Forgot password</Button>
-                </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-2.5">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="mail@example.com" {...field} />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="••••••••••••••••" {...field} />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                {error && <FormMessage>{error}</FormMessage>}
+                <Button variant="secondary">
+                    {isPending ? <Loader2/> : "Login"}
+                </Button>
             </form>
-        </>
+        </Form>
     )
 }
--- frontend/src/pages/frontPage/userTabs/registerTab.tsx
+++ frontend/src/pages/frontPage/userTabs/registerTab.tsx
@@ -1,16 +1,74 @@
 import {Input} from "@/components/ui/input.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.tsx";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const FormSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+        .min(6, "Password must be at least 6 characters long")
+        .refine((password) => /[A-Z]/.test(password), {
+            message: "Password must contain at least one uppercase character",
+        })
+        .refine((password) => /[a-z]/.test(password), {
+            message: "Password must contain at least one lowercase character",
+        })
+        .refine((password) => /[0-9]/.test(password), {
+            message: "Password must contain at least one digit",
+        })
+        .refine((password) => /\W|_/.test(password), {
+            message: "Password must contain at least one non-alphanumeric character",
+        })
+});
 
 export default function RegisterTab() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    })
+
+    function onsubmit(data: z.infer<typeof FormSchema>) {
+        console.log(data);
+    }
+
     return (
-        <>
-            <form className="space-y-2.5">
-                <Input placeholder="Email" inputMode="numeric"/>
-                <Input placeholder="Password" type="password"/>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onsubmit)} className="space-y-2.5">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="mail@example.com" {...field} />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="••••••••••••••••" {...field} />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
                 <Button variant="secondary">
                     Register
                 </Button>
             </form>
-        </>
+        </Form>
     )
 }
