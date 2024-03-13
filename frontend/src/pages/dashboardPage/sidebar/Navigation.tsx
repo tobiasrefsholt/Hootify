df6c06f697c14df1045e69d661ce868f96cf536b
tobias <tobias@refsholt.net>
2024-03-13 16:58:27 +0100

    Navigation design

--- frontend/src/pages/dashboardPage/sidebar/Navigation.tsx
+++ frontend/src/pages/dashboardPage/sidebar/Navigation.tsx
@@ -1,28 +1,46 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+import {Home} from "lucide-react";
+import {Separator} from "@/components/ui/separator.tsx";
+import {cn} from "@/lib/utils.ts";
+import {HTMLProps, ReactNode} from "react";
 
 export default function Navigation() {
     return (
         <nav>
             <ul className="flex flex-col gap-2.5">
-                <li>
-                    <Link to="/dashboard" className="text-white">Dashboard</Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/games" className="text-white">Games</Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/quizzes" className="text-white">Quizzes</Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/questions" className="text-white">Questions</Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/categories" className="text-white">Categories</Link>
-                </li>
-                <li>
-                    <Link to="/" className="text-white">Exit dashboard</Link>
-                </li>
+                <NavigationItem to={"/dashboard"}>Dashboard</NavigationItem>
+                <NavigationItem to={"/dashboard/games"}>Games</NavigationItem>
+                <NavigationItem to={"/dashboard/quizzes"}>Quizzes</NavigationItem>
+                <NavigationItem to={"/dashboard/questions"}>Questions</NavigationItem>
+                <NavigationItem to={"/dashboard/categories"}>Categories</NavigationItem>
+                <Separator className="my-5"/>
+                <NavigationItem to={"/"} icon={<Home className="h-4 w-4"/>}>Exit to frontpage</NavigationItem>
             </ul>
         </nav>
+    )
+}
+
+type NavigationItemProps = {
+    to: string;
+    icon?: ReactNode;
+    children: ReactNode;
+};
+
+function NavigationItem({to, icon, children}: NavigationItemProps) {
+    const baseClasses: HTMLProps<HTMLElement>["className"] = "flex items-center gap-2.5 px-2.5 py-1 rounded-md";
+    return (
+        <li>
+            <NavLink
+                to={to}
+                end
+                className={({isActive}) => isActive
+                    ? cn(baseClasses, "text-primary-foreground bg-secondary")
+                    : cn(baseClasses, "text-muted-foreground")
+                }
+            >
+                {icon}
+                {children}
+            </NavLink>
+        </li>
     )
 }
