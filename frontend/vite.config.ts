d7b945718d114a0e40f72b22e9d8a503eba05f53
tobias <tobias@refsholt.net>
2024-03-14 22:25:01 +0100

    Gzip compression on production build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import compression from "vite-plugin-compression2";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      compression()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
