b2e23787f32e73b983dd897683a783859f2f33fe
tobias <tobias@refsholt.net>
2024-03-15 10:40:23 +0100

    Implemented two-factor authentication

--- frontend/src/pages/dashboardPage/account/securityTab/twoFactorCard.tsx
+++ frontend/src/pages/dashboardPage/account/securityTab/twoFactorCard.tsx
@@ -1,9 +1,137 @@
-import {Card, CardHeader} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardHeader} from "@/components/ui/card.tsx";
+import {useFetch} from "@/hooks/useFetch.ts";
+import {ApiEndpoint} from "@/Types.ts";
+import {useEffect, useMemo} from "react";
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel, FormMessage,
+} from "@/components/ui/form"
+import {Switch} from "@/components/ui/switch"
+import {InputOTP, InputOTPGroup, InputOTPSlot} from "@/components/ui/input-otp.tsx";
+import {Button} from "@/components/ui/button.tsx";
+
+const FormSchema = z.object({
+    enable: z.boolean().default(false).optional(),
+    twoFactorCode: z.string().optional(),
+})
+
+type TwoFactorResponse = {
+    sharedKey: string,
+    recoveryCodesLeft: number,
+    recoveryCodes: string[],
+    isTwoFactorEnabled: boolean,
+    isMachineRemembered: boolean
+}
 
 export default function TwoFactorCard() {
+    const {data, doFetch} = useFetch<TwoFactorResponse>(ApiEndpoint.ManageTwoFactor, []);
+
+    useEffect(() => {
+        doFetch("POST", [], {});
+    }, []);
+
+    useEffect(() => {
+        form.reset({
+            enable: data?.isTwoFactorEnabled || false,
+            twoFactorCode: ""
+        });
+    }, [data]);
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: useMemo(() => {
+            return {
+                enable: data?.isTwoFactorEnabled || false,
+                twoFactorCode: ""
+            }
+        }, [data])
+    })
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        doFetch("POST", [], data);
+    }
+
+
     return (
         <Card>
             <CardHeader>Two factor authentication</CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2.5">
+                        <FormField
+                            control={form.control}
+                            name="enable"
+                            render={({field}) => (
+                                <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                                    <div className="space-y-0.5">
+                                        <FormLabel className="text-base">
+                                            Enable
+                                        </FormLabel>
+                                        <FormDescription>
+                                            Enable two factor authentication
+                                        </FormDescription>
+                                    </div>
+                                    <FormControl>
+                                        <Switch
+                                            checked={field.value}
+                                            onCheckedChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        {(form.watch("enable") === true) && data?.isTwoFactorEnabled === false &&
+                            <div className="rounded-lg border p-4 space-y-2.5">
+                                <FormField
+                                    control={form.control}
+                                    name="twoFactorCode"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormLabel>Enter one-time code</FormLabel>
+                                            <FormDescription>
+                                                Scan the QR code or copy the secret to your authenticator app
+                                            </FormDescription>
+                                            <FormControl>
+                                                <InputOTP
+                                                    {...field}
+                                                    autoFocus={true}
+                                                    maxLength={6}
+                                                    render={({slots}) => (
+                                                        <>
+                                                            <InputOTPGroup>
+                                                                {slots.map((slot, index) => (
+                                                                    <InputOTPSlot key={index} {...slot} />
+                                                                ))}{" "}
+                                                            </InputOTPGroup>
+                                                        </>
+                                                    )}
+                                                />
+                                            </FormControl>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <div className="space-x-2.5 mt-2.5">
+                                    <Button variant="secondary">Show QR-code</Button>
+                                    <Button
+                                        variant="outline"
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            navigator.clipboard.writeText(data?.sharedKey || "")
+                                        }}
+                                    >Copy secret</Button>
+                                </div>
+                            </div>}
+                        {form.formState.isDirty && <Button type="submit">Save</Button>}
+                    </form>
+                </Form>
+            </CardContent>
         </Card>
     )
 }
--- frontend/src/pages/frontPage/userTabs/loginTab.tsx
+++ frontend/src/pages/frontPage/userTabs/loginTab.tsx
@@ -5,14 +5,30 @@ import {useUser} from "@/context/userContext.tsx";
 import {useNavigate} from "react-router";
 import {Loader2} from "lucide-react";
 import {ApiEndpoint} from "@/Types.ts";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.tsx";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form.tsx";
 import {useForm} from "react-hook-form";
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
+import {InputOTP, InputOTPGroup, InputOTPSlot} from "@/components/ui/input-otp.tsx";
+
+type RegisterErrorResponse = {
+    type: string;
+    title: string;
+    status: number;
+    detail: string;
+}
 
 const FormSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(1, "Password cannot be empty")
+    password: z.string().min(1, "Password cannot be empty"),
+    twoFactorCode: z.string().optional()
 });
 
 export default function LoginTab() {
@@ -20,19 +36,19 @@ export default function LoginTab() {
     const navigate = useNavigate();
 
     const [isPending, setIsPending] = useState(false);
-    const [error, setError] = useState<string | null>(null);
+    const [useTwoFactor, setUseTwoFactor] = useState(false);
 
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: {
             email: "",
-            password: ""
+            password: "",
+            twoFactorCode: ""
         }
     })
 
     async function handleLogin(data: z.infer<typeof FormSchema>) {
         setIsPending(true);
-        setError(null);
 
         try {
             const response = await fetch(import.meta.env.VITE_BACKEND_URL + ApiEndpoint.Login, {
@@ -44,54 +60,101 @@ export default function LoginTab() {
                 body: JSON.stringify(data)
             });
             setIsPending(false);
-            if (response.status === 401) {
-                setError("Wrong username or password.");
+
+            // Handle successful response
+            if (response.ok) {
+                user.fetch();
+                navigate("/dashboard");
                 return;
             }
-            if (response.status !== 200) {
-                throw new Error();
+
+            // Handle error response
+            const json: RegisterErrorResponse | null = await response.json();
+            switch (json?.detail) {
+                case "Failed":
+                    form.setError(
+                        "root",
+                        useTwoFactor
+                            ? {message: "Invalid one-time code"}
+                            : {message: "Invalid email or password"}
+                    )
+                    break;
+                case "RequiresTwoFactor":
+                    useTwoFactor
+                        ? form.setError("twoFactorCode", {message: "Two factor authentication required"})
+                        : setUseTwoFactor(true);
+                    break;
+                default:
+                    throw new Error();
             }
+
         } catch (e) {
-            setError("Something went wrong. Please try again later.");
+            form.setError("root", {
+                message: "Something went wrong. Please try again later."
+            })
             setIsPending(false);
             return;
         }
-
-        setIsPending(false);
-        user.fetch();
-        navigate("/dashboard");
     }
 
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-2.5">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="mail@example.com" {...field} />
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="password"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input type="password" placeholder="••••••••••••••••" {...field} />
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
-                {error && <FormMessage>{error}</FormMessage>}
+                {useTwoFactor ?
+                    <FormField
+                        control={form.control}
+                        name="twoFactorCode"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Enter one-time code</FormLabel>
+                                <FormControl>
+                                    <InputOTP
+                                        {...field}
+                                        autoFocus={true}
+                                        maxLength={6}
+                                        render={({slots}) => (
+                                            <InputOTPGroup>
+                                                {slots.map((slot, index) => (
+                                                    <InputOTPSlot key={index} {...slot} />
+                                                ))}{" "}
+                                            </InputOTPGroup>
+                                        )}
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    : <>
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="mail@example.com" {...field} />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="••••••••••••••••" {...field} />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                    </>}
+                {form.formState.errors.root?.message &&
+                    <FormMessage>{form.formState.errors.root?.message}</FormMessage>}
                 <Button variant="secondary">
                     {isPending ? <Loader2/> : "Login"}
                 </Button>
